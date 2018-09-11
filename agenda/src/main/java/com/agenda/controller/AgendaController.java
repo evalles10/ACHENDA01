bf70a1dcd238b4f82e6a6b96c70cfe2de73254a1
GuillermoDG <diazgarozguillermo@gmail.com>
2018-09-11 14:28:59 +0200

    Controller done

--- agenda/src/main/java/com/agenda/controller/AgendaController.java
+++ agenda/src/main/java/com/agenda/controller/AgendaController.java
@@ -6,20 +6,70 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.agenda.model.*;
 import com.agenda.services.*;
+import com.agenda.model.*;
 
 @Controller
 public class AgendaController {
 
 	@Autowired
-	private AgendaService agendaService;
+	private EmpleadoService empleadoService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(AgendaController.class);
 	
+
 	@RequestMapping("/")
+	public ModelAndView handleRequest() throws Exception {
+		logger.info("--en Listado");
+		List<empleados> listEmpleados = empleadoService.list();
+		ModelAndView model = new ModelAndView("EmpleadoList");
+		model.addObject("empleadoList", listEmpleados);
+		return model;
+	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public ModelAndView newEmpleado() {
+		logger.info("-- en NEW");
+		ModelAndView model = new ModelAndView("EmpleadoForm");
+		model.addObject("empleado", new Empleado());
+		return model;		
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView editEmpleado(HttpServletRequest request) {
+		logger.info("-- en EDIT");
+		int IdEmpleado = Integer.parseInt(request.getParameter("idEmpleado"));
+		Empleado empleado = empleadoService.get(IdEmpleado);
+		ModelAndView model = new ModelAndView("UserForm");
+		model.addObject("empleado", empleado);
+		return model;		
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public ModelAndView deleteEmpleado(HttpServletRequest request) {
+		logger.info("-- en DELETE");
+		int IdEmpleado = Integer.parseInt(request.getParameter("idEmpleado"));
+		empleadoService.delete(IdEmpleado);
+		return new ModelAndView("redirect:/");		
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView saveEmpleado(@ModelAttribute Empleado empleado) {
+		logger.info("-- en SAVE");
+		empleadoService.add(empleado);
+		return new ModelAndView("redirect:/");
+	}
+	
+	
+	
+	
+	
 	
 }
