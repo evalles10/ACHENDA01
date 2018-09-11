d8b04aeed0535593fec24ae9c48ba9ae0f81c805
GuillermoDG <diazgarozguillermo@gmail.com>
2018-09-11 11:15:15 +0200

    Persona Hecha y derecha

--- agenda/src/main/java/com/agenda/model/Persona.java
+++ agenda/src/main/java/com/agenda/model/Persona.java
@@ -3,16 +3,25 @@ package com.agenda.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-@Entity
-@Table(name="PERSONAS")
-public class Persona implements Serializable{
+import com.agenda.model.Empleado;
 
+@Entity
+@Table(name = "PERSONAS")
+public class Persona implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idpersonas;
@@ -20,10 +29,79 @@ public class Persona implements Serializable{
 	private String apellido1;
 	private String apellido2;
 	private String dni;
-	private Date fechaNacimiento; // Mi gente esto hay que discutir con dios si usar Date o calendar porque huele a chotuno
-	
-									//Mi gente otra vez, faltan setters y getters porque hay que meter el FK y luego se nos va a olvidar
-	
-	
-	
+	private Date fechaNacimiento;
+
+	@OneToOne(mappedBy = "idEmpleado", cascade = CascadeType.ALL)
+	@JoinColumn(name = "idEmpleado")
+	private Empleado empleado;
+
+	public Persona() {
+		super();
+	}
+
+	public Persona(Integer idpersonas, String nombre, String apellido1, String apellido2, String dni,
+			Date fechaNacimiento) {
+		super();
+		this.idpersonas = idpersonas;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.dni = dni;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Integer getIdpersonas() {
+		return idpersonas;
+	}
+
+	public void setIdpersonas(Integer idpersonas) {
+		this.idpersonas = idpersonas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [idpersonas=" + idpersonas + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2="
+				+ apellido2 + ", dni=" + dni + ", fechaNacimiento=" + fechaNacimiento + "]";
+	}
+
 }
