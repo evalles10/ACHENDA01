3d947c904e119486432958d0fe84f1aa87e8f9cb
evalles10 <enriquevalles_1@hotmail.com>
2018-09-11 15:28:18 +0200

    EmpleadoServiceImpl

--- agenda/src/main/java/com/agenda/services/EmpleadoServiceImpl.java
+++ agenda/src/main/java/com/agenda/services/EmpleadoServiceImpl.java
@@ -1,10 +1,57 @@
 package com.agenda.services;
 
-public class EmpleadoServiceImpl {
+import java.util.List;
 
-	
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.agenda.model.Empleado;
+
+
+@Service
+@Transactional
+public class EmpleadoServiceImpl implements EmpleadoService {
+
+	@Autowired
+	private EmpleadoService empleadoService;
 	
 	public void ListarEmpleados(){
+		
+	}
+	
+	public EmpleadoServiceImpl(EmpleadoService empleadoService) {
+		super();
+		this.empleadoService = empleadoService;
+	}
+
+	@Override
+	public List<Empleado> list() {
+		
+		return empleadoService.list();
+	}
+
+	@Override
+	public Empleado get(int idEmpleado) {
+		return empleadoService.get(idEmpleado);
+	}
+
+	@Override
+	public void update(Empleado empleado) {
+		empleadoService.update(empleado);
+		
+	}
+
+	@Override
+	public void add(Empleado empleado) {
+		empleadoService.add(empleado);
+		
+	}
+
+	@Override
+	public void delete(int idEmpleado) {
+		empleadoService.delete(idEmpleado);
 		
 	}
 }
