1479977b73d8896c31ba90954cde856c56b9d547
GuillermoDG <diazgarozguillermo@gmail.com>
2018-09-11 11:35:29 +0200

    Clase Direccion Hecha

---
+++ agenda/src/main/java/com/agenda/model/Direccion.java
@@ -0,0 +1,94 @@
+package com.agenda.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.agenda.model.Persona;
+
+@Entity
+@Table(name = "DIRECCIONES")
+public class Direccion implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer iddirecciones;
+	private String direccion;
+	private String codPostal;
+	private String localidad;
+	private String provincia;
+
+	@OneToOne(mappedBy = "idPersona", cascade = CascadeType.ALL)
+	@JoinColumn(name = "idPersona")
+	private Persona persona;
+
+	public Direccion() {
+		super();
+	}
+
+	public Direccion(Integer iddirecciones, String direccion, String codPostal, String localidad, String provincia) {
+		super();
+		this.iddirecciones = iddirecciones;
+		this.direccion = direccion;
+		this.codPostal = codPostal;
+		this.localidad = localidad;
+		this.provincia = provincia;
+	}
+
+	public Integer getIddirecciones() {
+		return iddirecciones;
+	}
+
+	public void setIddirecciones(Integer iddirecciones) {
+		this.iddirecciones = iddirecciones;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCodPostal() {
+		return codPostal;
+	}
+
+	public void setCodPostal(String codPostal) {
+		this.codPostal = codPostal;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	@Override
+	public String toString() {
+		return "Direccion [iddirecciones=" + iddirecciones + ", direccion=" + direccion + ", codPostal=" + codPostal
+				+ ", localidad=" + localidad + ", provincia=" + provincia + "]";
+	}
+	
+	
+
+}
