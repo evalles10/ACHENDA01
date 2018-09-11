ba48f0b10d6f45f5eb2e665609f0232d913e3f8c
Dal <Dalvin20099@gmail.com>
2018-09-11 11:35:12 +0200

    Clase Empleado
    Creacion de la clase Empleado

--- agenda/src/main/java/com/agenda/model/Empleado.java
+++ agenda/src/main/java/com/agenda/model/Empleado.java
@@ -1,17 +1,20 @@
 package com.agenda.model;
-
+import com.agenda.model.Departamento;
 import java.io.Serializable;
 import java.sql.Date;
 import java.sql.Timestamp;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
-@Table(name="Empleado")
+@Table(name="EMPLEADO")
 public class Empleado implements Serializable {
 
 	@Id
@@ -20,6 +23,68 @@ public class Empleado implements Serializable {
 	private Integer codEmpleado;
 	private Integer salario;
 	private Timestamp fechaAlta;
+	
+	@OneToOne(mappedBy = "idDepartamento",cascade = CascadeType.ALL)
+	@JoinColumn(name = "iDepartamento")
+	private Departamento departamento;
+	
+	@OneToOne(mappedBy = "idCategoria",cascade = CascadeType.ALL)
+	@JoinColumn(name = "idCategoria")
+	private Categoria categoria;
+	
+
+	public Empleado() {
+		super();
+	}
+
+	public Empleado(Integer idEmpleados, Integer codEmpleado, Integer salario, Timestamp fechaAlta, Empleado empleado) {
+		super();
+		this.idEmpleados = idEmpleados;
+		this.codEmpleado = codEmpleado;
+		this.salario = salario;
+		this.fechaAlta = fechaAlta;
+	
+	}
+
+	public Integer getIdEmpleados() {
+		return idEmpleados;
+	}
+
+	public void setIdEmpleados(Integer idEmpleados) {
+		this.idEmpleados = idEmpleados;
+	}
+
+	public Integer getCodEmpleado() {
+		return codEmpleado;
+	}
+
+	public void setCodEmpleado(Integer codEmpleado) {
+		this.codEmpleado = codEmpleado;
+	}
+
+	public Integer getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Integer salario) {
+		this.salario = salario;
+	}
+
+	public Timestamp getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Timestamp fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [idEmpleados=" + idEmpleados + ", codEmpleado=" + codEmpleado + ", salario=" + salario
+				+ ", fechaAlta=" + fechaAlta + "]";
+	}
+	
+	
 	
 
 }
